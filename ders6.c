6beda7db0dd6b75e426378be96cf522faba27188
msait <saidgokoglans8@gmail.com>
2022-11-05 12:24:52 +0300

    Ders6 printf örnekleri
    %d %f \n kullanim örnekleri

---
+++ ders6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	int x=3;
+	float y=0.1;
+	
+	printf("%f\n",x/y);
+	
+	printf("%f\n", 4.2*2 + 4%2);
+	printf("%d \n",(int)3.2 );
+	printf("%f \n",(float)4);
+	
+	return 0;
+}
